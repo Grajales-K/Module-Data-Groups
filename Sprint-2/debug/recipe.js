dfa4be4f76876a98fdaac712c9bfe44de8f402e6
Karla G. <k.grajalesluna@gmail.com>
2025-01-16 16:56:03 +0000

    refactor fucntion with join method

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,16 +4,23 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
-const recipe = {
-  title: "bruschetta",
-  serves: 2,
-  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
-};
+// const recipe = {
+//   title: "bruschetta",
+//   serves: 2,
+//   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+// };
+
+// console.log(`${recipe.title} serves ${recipe.serves}`);
+
+// let ingredients = recipe["ingredients"];
+
+// for (let i = 0; i < ingredients.length; i++) {
+//   console.log(ingredients[i]);
+// }
+
+// ==================== one  line with join method ====================
 
 console.log(`${recipe.title} serves ${recipe.serves}`);
 
-let ingredients = recipe["ingredients"];
-
-for (let i = 0; i < ingredients.length; i++) {
-  console.log(ingredients[i]);
-}
+// Display the ingredients in one line with a new line (\n) between each ingredient
+console.log(recipe.ingredients.join("\n"));
